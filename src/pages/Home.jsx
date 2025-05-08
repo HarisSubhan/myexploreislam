f7da11c12794bdcefd62c95cdc45f0c97fb6badb
HARIS SUBHAN <jackieliam7@gmail.com>
2025-05-09 02:40:24 +0500

    header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "../components/Header";
+
+const Home = () => (
+  <>
+    <Header />
+  </>
+);
+
+export default Home;
