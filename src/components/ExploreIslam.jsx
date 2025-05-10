338eba9d237a0c601834c89cb725675b4761bf79
HarisSubhan <jackieliam7@gmail.com>
2025-05-10 11:46:18 +0500

    header update

---
+++ src/components/ExploreIslam.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const ExploreIslam = () => {
+  return (
+    <>
+    <Container>
+        <Row>
+            <Col>
+            <p className="display-3 fw-bold text-uppercase mb-3" style={{ color: "#2a5f73" }}>
+  MARYAM & MUAZ
+</p>
+            </Col>
+            <Col></Col>
+        </Row>
+    </Container>
+    </>
+  )
+}
+
+export default ExploreIslam
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import ExploreIslam from "../components/ExploreIslam";
 
 const Home = () => (
   <>
     <Header />
+    <HeroSection/>
+    <ExploreIslam/>
   </>
 );
 
