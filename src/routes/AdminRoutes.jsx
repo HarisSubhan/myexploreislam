8b13a14252f9067f9381a08cb2902f399bd03a2c
HARIS SUBHAN <jackieliam7@gmail.com>
2025-05-27 01:49:36 +0500

    color changing theme

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-// import AdminApp from "../pages/AdminPortal/AdminApp";
-// import Dashboard from "./pages/Dashboard";
-// import ManageVideos from "./pages/ManageVideos";
+import AdminApp from "../pages/AdminPortal/AdminApp";
+import Dashboard from "../pages/AdminPortal/pages/Dashboard"
+
+
 
 const AdminRoutes = () => (
   <Routes>
     <Route path="/" element={<AdminApp />}>
-      {/* <Route index element={<Dashboard />} /> */}
+      <Route index element={<Dashboard />} />
       {/* <Route path="videos" element={<ManageVideos />} /> */}
     </Route>
   </Routes>
