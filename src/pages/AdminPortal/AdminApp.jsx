83cb928a2a5b3512219cae0915bae421af48b29a
wasehabbasi <abdulwasehabbasi@hotmail.com>
2025-05-27 14:41:37 +0500

    dashboard setting

--- src/pages/AdminPortal/AdminApp.jsx
+++ src/pages/AdminPortal/AdminApp.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./pages/components/Header";
-
+import Header from '../../../src/components/admin/Header';
+import Sidebar from "../../../src/components/admin/Sidebar";
 
 const AdminApp = () => {
   return (
-    <>
-      <Header />
-      <main style={{ padding: "1rem" }}>
-        <Outlet />
-      </main>
-    </>
+    <div className="d-flex">
+      {/* Left Sidebar */}
+      <Sidebar />
+
+      {/* Main Area */}
+      <div style={{ marginLeft: "250px", width: "100%" }}>
+        <Header />
+        <main style={{ padding: "1rem" }}>
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
