9c84b2beb18f7a3352f4987aaaf97896d2015c8d
Abdul Waseh <abdulwasehabbasi@hotmail.com>
2025-06-02 02:35:02 +0500

    working on assignments, blogs, books, quizzes, videos and there structure

--- src/pages/AdminPortal/AdminApp.jsx
+++ src/pages/AdminPortal/AdminApp.jsx
@@ -1,32 +1,3 @@
-// import React from "react";
-// import { Outlet } from "react-router-dom";
-// import Header from '../../../src/components/admin/Header';
-// import Sidebar from "../../../src/components/admin/Sidebar";
-// import StatCard from "../../../src/components/admin/StatCard";
-
-// const AdminApp = ({ children }) => {
-//   return (
-//     <div className="d-flex">
-//       {/* Left Sidebar */}
-//       <Sidebar />
-
-//       {/* Main Area */}
-//       <div style={{ width: "100%" }}>
-//         <Header />
-//         <main style={{ padding: "1rem" }}>
-//           <Outlet />
-//           <StatCard/>
-
-//           {/* {children} */}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AdminApp;
-
-
 import React from "react";
 import Sidebar from "../../components/admin/Sidebar";
 import Header from "../../components/admin/Header";
