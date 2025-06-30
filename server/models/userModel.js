0117e66a1a4f18921726d2ac036a9d26ddaad501
Abdul Waseh <abdulwasehabbasi@hotmail.com>
2025-06-30 22:41:28 +0500

    worked on admin setting API's

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,8 @@
 const db = require('../config/db');
 
+const bcrypt = require('bcryptjs');
+
+
 const createUser = (name, email, password, role, callback) => {
   const sql = 'INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)';
   db.query(sql, [name, email, password, role], callback);
@@ -10,4 +13,24 @@ const findUserByEmail = (email, callback) => {
   db.query(sql, [email], callback);
 };
 
-module.exports = { createUser, findUserByEmail };
+const getById = (id, callback) => {
+  db.query(`SELECT * FROM users WHERE id = ?`, [id], callback);
+};
+
+const updateByID = (id, name, email, password, callback) => {
+  let sql = `UPDATE users SET name = ?, email = ?`;
+  const params = [name, email];
+
+  if (password) {
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    sql += `, password = ?`;
+    params.push(hashedPassword);
+  }
+
+  sql += ` WHERE id = ? AND role = 'admin'`;
+  params.push(id);
+
+  db.query(sql, params, callback);
+}
+
+module.exports = { createUser, findUserByEmail, getById, updateByID  };
