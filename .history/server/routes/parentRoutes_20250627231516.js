d787bc5bccb840b4547fa35b8e8a4c25011b0a3b
haris <jackieliam7@gmail.com>
2025-07-21 13:24:08 +0500

    deleted history folder

--- .history/server/routes/parentRoutes_20250627231516.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middleware/auth');
-const { addChild } = require('../controllers/parentController');
-
-router.post('/add-child', verifyToken, addChild);
-
-module.exports = router;
