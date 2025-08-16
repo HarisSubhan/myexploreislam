86138a0a20dacae01b03c3a1f63a696fd00d643e
wasehabbasi <54438639+wasehabbasi@users.noreply.github.com>
2025-08-17 01:41:38 +0500

    subscription for pulic

--- server/routes/subscriptionRoutes.js
+++ server/routes/subscriptionRoutes.js
@@ -5,11 +5,12 @@ const isAdmin = require('../middleware/isAdmin');
 const subscriptionController = require('../controllers/subscriptionController');
 
 // Parent routes
+router.get('/all_active', subscriptionController.getAllActiveSubscriptions);
 router.post('/subscribe', verifyToken, subscriptionController.subscribe);
 router.get('/me', verifyToken, subscriptionController.getMySubscription);
 router.get('/', verifyToken, subscriptionController.getAllSubscriptions);
 router.put('/cancel', verifyToken, subscriptionController.cancelSubscription);
-router.get('/all_active', verifyToken, subscriptionController.getAllActiveSubscriptions);
+
 
 // Admin routes
 router.get('/admin/all', verifyToken, isAdmin, subscriptionController.getAllSubscriptions);
