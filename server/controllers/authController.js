27aca08b194388ecaf7c0cb60046dd5ec54c2d1a
wasehabbasi <54438639+wasehabbasi@users.noreply.github.com>
2025-11-20 19:20:54 +0500

    reverse child object and also add age and active

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -197,7 +197,14 @@ const login = (req, res) => {
               res.json({
                 message: 'Login successful',
                 token,
-                user: child
+                user: {
+                  id: child.id,
+                  name: child.name,
+                  username: child.username,
+                  email: child.email,
+                  role: 'child',
+                  parent_id: child.parent_id
+                }
               });
             });
           }
