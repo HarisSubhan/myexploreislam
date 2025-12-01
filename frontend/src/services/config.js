2c46880b206fbae585eee42fca5210042ac965af
Haris Subhan <jackieliam7@gmail.com>
2025-12-01 14:21:56 +0500

    delopy

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,5 +1,7 @@
 export const baseUrl =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  // import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "http://myexploreislam.com";
+
 
 export const getToken = () => localStorage.getItem("token");
 
