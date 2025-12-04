ee1af8c1ce3cbcc30d008395311aad5a6daabb37
Haris Subhan <jackieliam7@gmail.com>
2025-12-04 18:33:15 +0500

    all done

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,6 +1,6 @@
 export const baseUrl =
-  // import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-  import.meta.env.VITE_API_BASE_URL || "http://myexploreislam.com";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  // import.meta.env.VITE_API_BASE_URL || "http://myexploreislam.com";
 
 
 export const getToken = () => localStorage.getItem("token");
