763bb11c304e0c046ad09eb059bb5735bebd3a60
Haris Subhan <jackieliam7@gmail.com>
2025-12-09 13:44:52 +0500

    db update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:5000',
-        // target: 'http://myexploreislam.com',
+        // target: 'http://localhost:5000',
+        target: 'http://myexploreislam.com',
         changeOrigin: true,
         secure: false,
       }
